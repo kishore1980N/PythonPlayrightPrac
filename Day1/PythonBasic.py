e2540254d4679c86943c06eccb1abee934d2247f
Kishore1980N <kishore1980naidu@gmail.com>
2024-10-20 10:34:55 -0600

    first commit

---
+++ Day1/PythonBasic.py
@@ -0,0 +1,20 @@
+from operator import truediv
+
+print("hello world")
+name = "Kishore"
+print(name)
+print(type(name))
+age = 25
+print(age)
+print(type(age))
+weight = 60.01
+print(weight)
+print(type(weight))
+wfh = True
+#print(wfh)
+print(type(wfh))
+"""
+This is a sample python code
+This is a test
+"""
+print(len(name))
